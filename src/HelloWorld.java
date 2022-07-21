f9e480a640ac859617cc945a2477bd7539718d65
Agung-Hernawan <agung.h@usd.ac.id>
2022-07-21 11:29:48 +0700

    Balik

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World...");
         System.out.println("Tambah Baris 1");
-        System.out.println("Tambah yang ke 2");
     }
 }
