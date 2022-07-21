78b9e4f1ffde987bdcb38c5fd87669813b193be4
Agung-Hernawan <agung.h@usd.ac.id>
2022-07-21 11:18:17 +0700

    Tambah yang ke 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World...");
         System.out.println("Tambah Baris 1");
+        System.out.println("Tambah yang ke 2");
     }
 }
